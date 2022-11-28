1d4872541b8d60ddb8d2bab6050ceb084c2a9c7e
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 14:43:18 +0530

    Add:Ability to delete a person using persons name

---
+++ src/main/java/com/bridgelabz/addressbooksystem/addressbooksystemUC4/AddressbooksystemUc4Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbooksystem.addressbooksystemUC4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbooksystemUc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressbooksystemUc4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/addressbooksystem/addressbooksystemUC4/AddressbooksystemUc4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbooksystem.addressbooksystemUC4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbooksystemUc4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
