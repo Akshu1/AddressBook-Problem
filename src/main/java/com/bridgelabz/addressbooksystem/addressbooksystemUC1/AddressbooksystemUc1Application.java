3e1fefcb6bb3c5ff47fb92c35f42087a4d9dd23d
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 14:23:57 +0530

    Added:To display Welcome

---
+++ src/main/java/com/bridgelabz/addressbooksystem/addressbooksystemUC1/AddressbooksystemUc1Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbooksystem.addressbooksystemUC1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbooksystemUc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressbooksystemUc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/addressbooksystem/addressbooksystemUC1/AddressbooksystemUc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbooksystem.addressbooksystemUC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbooksystemUc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
