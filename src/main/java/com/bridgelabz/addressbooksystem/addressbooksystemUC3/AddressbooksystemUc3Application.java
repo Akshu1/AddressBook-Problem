0fd7d27cebfcfd1157f806422a3a55410bb12e2a
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 14:38:22 +0530

    Add:Ability to edit existing customer using name

---
+++ src/main/java/com/bridgelabz/addressbooksystem/addressbooksystemUC3/AddressbooksystemUc3Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbooksystem.addressbooksystemUC3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbooksystemUc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressbooksystemUc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/addressbooksystem/addressbooksystemUC3/AddressbooksystemUc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbooksystem.addressbooksystemUC3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbooksystemUc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
